7c4086b472e452efc554a27f4e72e9b9ce810c6a
Jean Mercadier <jeanmercadier@live.com>
2014-07-04 11:54:25 +0200

    revu design

--- ownpage.js
+++ ownpage.js
@@ -7,81 +7,99 @@
 * - Attribution requires leaving author name, author link, and the license info intact
 */
 
-(function () {
-	var draw, edit;
+$(document).ready(function () {
+	if($(location).attr('hash') == "#clear"){
+		if(confirm("Are you sure to erase your local data?\nSaves will be lost.")){
+			localStorage.clear();
+		}
+		$(location).attr('hash',"")
+	}
+
+	var draw, edit, urls;
+
+	try {
+		if(!('localStorage' in window && window['localStorage'] !== null)){
+			alert("Your browser does not support local storage (HTML5). :(");
+			return;
+		}
+	} catch (e) {
+		alert("Your browser does not support local storage (HTML5). :(");
+		return;
+	}
+	if(localStorage.getItem("urls")){
+		urls = JSON.parse(localStorage.getItem("urls"));
+	}
+	else {
+		$.getJSON("ownpage.json",function($content){
+			urls = $content["urls"];
+			localStorage.setItem("urls", JSON.stringify($content["urls"]));
+		});
+	}
 
 	draw = (function () {
-		$.getJSON("ownpage.json",function($pages){
-			$("#edition").hide();
-			$("#marquespages").empty();
-			$("#marquespages").show();
-			$.each($pages,function($i,$range){
-				$ligne = $("<tr></tr>");
-				$j = 0;
-				$.each($range,function($nom,$cont){
-					$cell = $("<td id='t" + $i + $j + "'>" + $nom + "</td>");
-					$cell.click(function(){
-						$(location).attr('href',$cont[0]);
-					});
-					$cell.css({ "background-color": $cont[1] });
-					$cell.appendTo($ligne);
-					$j += 1;
+		$("#edition").hide();
+		$("#marquespages").empty();
+		$("#marquespages").show();
+		$.each(urls,function($row,$range){
+			$ligne = $("<tr></tr>");
+			$col = 0;
+			$.each($range,function($nom,$cont){
+				$cell = $("<td id='t" + $row + $col + "'>" + $nom + "</td>");
+				$cell.click(function(){
+					$(location).attr('href',$cont[0]);
 				});
-				$ligne.appendTo("#marquespages");
+				$cell.css({ "background-color": $cont[1] });
+				$cell.appendTo($ligne);
+				$col += 1;
 			});
-			$("#edit").html("EDIT");
-			$("#edit").click(function(){
-				edit.call(this);
-			});
+			$ligne.appendTo("#marquespages");
+		});
+		$("#edit").html("EDIT");
+		$("#edit").click(function(){
+			edit.call(this);
 		});
 	});
 
 	edit = (function () {
-		$.getJSON("ownpage.json",function($pages){
-			$("#marquespages").hide();
-			$("#edition").empty();
-			$("#edition").show();
-			$.each($pages,function($i,$range){
-				$ligne = $("<tr></tr>");
-				$j = 0;
-				$.each($range,function($nom,$cont){
-					$cell  = $("<td id='e" + $i + $j + "'></td>");
-					$cell.appendTo($ligne);
-					$inom  = $("<input type='text' placeholder='Name' value='" + $nom + "' />");
-					$inom.change(function(){
-						alert("coucou");
-					});
-					$inom.appendTo($cell);
-					$ihref = $("<input type='text' placeholder='URL' value='" + $cont[0] + "' />");
-					$ihref.change(function(){
-						alert("coucou");
-					});
-					$ihref.appendTo($cell);
-					$colpick = $("<div class='color-box'></div>");
-					$colpick.colpick({
-						colorScheme:'dark',
-						layout:'rgbhex',
-						color: $cont[1],
-						onSubmit:function(hsb,hex,rgb,el) {
-							$(el).css('background-color', '#'+hex);
-							$(el).colpickHide();
-						}
-					}).css('background-color', $cont[1]);
-					$colpick.appendTo($cell);
-					$j += 1;
+		$("#marquespages").hide();
+		$("#edition").empty();
+		$("#edition").show();
+		$.each(urls,function($row,$range){
+			$ligne = $("<tr></tr>");
+			$col = 0;
+			$.each($range,function($nom,$cont){
+				$cell  = $("<td id='e" + $row + $col + "'></td>");
+				$cell.appendTo($ligne);
+				$inom  = $("<input type='text' placeholder='Name' value='" + $nom + "' />");
+				$inom.change(function(){
+					$old = urls[$row][$nom];
+					alert($old.ID);
 				});
-				$ligne.appendTo("#edition");
+				$inom.appendTo($cell);
+				$ihref = $("<input type='text' placeholder='URL' value='" + $cont[0] + "' />");
+				$ihref.change(function(){
+					alert("coucou");
+				});
+				$ihref.appendTo($cell);
+				$colpick = $("<div class='color-box'></div>");
+				$colpick.colpick({
+					colorScheme : 'light',
+					layout      : 'rgbhex',
+					color       : $cont[1],
+					onSubmit:function(hsb,hex,rgb,el) {
+						$(el).css('background-color', '#'+hex);
+						$(el).colpickHide();
+					}
+				}).css('background-color', $cont[1]);
+				$colpick.appendTo($cell);
+				$col += 1;
 			});
-			$("#edit").html("DONE");
-			$("#edit").click(function(){
-				draw.call(this);
-			});
+			$ligne.appendTo("#edition");
 		});
-		// $("#marquespages td").css({
-		// 	"background-color": "white",
-		// 	"border": "3px dashed #3a3a3a",
-		// 	"color": "#3a3a3a"
-		// });
+		$("#edit").html("DONE");
+		$("#edit").click(function(){
+			draw.call(this);
+		});
 	});
 
 	draw.call(this);
